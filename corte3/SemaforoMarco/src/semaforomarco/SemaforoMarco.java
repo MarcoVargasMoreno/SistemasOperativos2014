215e9109fe5629fb25e545d0ca91ab5e024bf1c0
MarcoVargas <mv972586@ingenieria.sanmartin.edu.co>
2014-12-18 09:46:42 -0500

    Se sube programa de semaforo

---
+++ corte3/SemaforoMarco/src/semaforomarco/SemaforoMarco.java
@@ -0,0 +1,12 @@
+package semaforomarco;
+
+
+public class SemaforoMarco {
+
+    
+    public static void main(String[] args) {
+        MiFormulario mf = new MiFormulario();
+        mf.setVisible(true);
+    }
+    
+}
