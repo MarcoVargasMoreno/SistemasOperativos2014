ddb054f5f17e07d573f7451c963316c6b96264cd
MarcoVargas <mv972586@ingenieria.sanmartin.edu.co>
2014-12-18 09:42:42 -0500

    Se sube programa de ejemplo basico de hilos

---
+++ corte3/EjemploBasicodeHilo/src/ejemplobasicodehilo/EjemploBasicodeHilo.java
@@ -0,0 +1,10 @@
+package ejemplobasicodehilo;
+
+public class EjemploBasicodeHilo {
+
+    public static void main(String[] args) {
+        Ventana v = new Ventana();
+        v.setVisible(true);
+    }
+    
+}
